4c9822bbde8c4ea4c815d997115a098fa52f386f
Kris Reid <krisreid_06@hotmail.com>
2024-03-11 21:20:02 +0000

    updated with model and id

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import requests
 import os
+import uuid
+from models.service import Service
 
 def get_json_files(repo_owner, repo_name, access_token, path=''):
 
     api_url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{path}"
     headers = {'Authorization': f'token {access_token}'}
 
+    services = []
+
     response = requests.get(api_url, headers=headers)
 
     if response.status_code == 200:
-        json_files = []
-
         # Parse response JSON
         contents = response.json()
-
         # Loop through each item in the response
         for item in contents:
             if item['type'] == 'file':
                 # Check if file has .json extension
                 if item['name'] == 'metadata.json':
-                    json_files.append(item['download_url'])
+                    # Make request and parse the response
+                    response = requests.get(item['download_url'])
+                    content = response.json()
+                    # Map the response to a Model
+                    new_service = Service(
+                        id=str(uuid.uuid4()),
+                        name=content['name'],
+                        owner=content['owner']
+                    )
+                    # append the service to an array
+                    services.append(new_service)
             elif item['type'] == 'dir':
                 # Recursively get JSON files from subdirectories
                 subdir_files = get_json_files(repo_owner, repo_name, access_token, f"{path}/{item['name']}")
-                json_files.extend(subdir_files)
-        return json_files
+                services.extend(subdir_files)
+        return services
     else:
         print(f"Failed to fetch repository contents. Status code: {response.status_code}")
         return None
@@ -40,11 +51,7 @@ if __name__ == "__main__":
 
     if json_files:
         print("JSON files found in the repository:")
-
         for json_file in json_files:
-            response = requests.get(json_file)
-            content = response.json()
-            print(content)
-
+            print(f"{json_file.id} \t {json_file.name} \t {json_file.owner}")
     else:
         print("No JSON files found in the repository.")
